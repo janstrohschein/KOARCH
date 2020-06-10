2afd2bab3c5d2da950ebeb01eb84fd89b9f4c99a
janstrohschein <strohschein@gmx.net>
2020-06-10 13:17:10 +0200

    python file formatted

--- Big Data Platform/Docker/src/1p_count_up.py
+++ Big Data Platform/Docker/src/1p_count_up.py
@@ -3,19 +3,20 @@ import time
 
 from classes.KafkaPC import KafkaPC
 
-print('start 1p_count_up')
+print("start 1p_count_up")
 
-env_vars = {'kafka_broker_url': os.getenv('KAFKA_BROKER_URL'),
-            'out_topic': os.getenv('OUT_TOPIC'),
-            'out_schema_file': os.getenv('OUT_SCHEMA_FILE')
-            }
+env_vars = {
+    "kafka_broker_url": os.getenv("KAFKA_BROKER_URL"),
+    "out_topic": os.getenv("OUT_TOPIC"),
+    "out_schema_file": os.getenv("OUT_SCHEMA_FILE"),
+}
 
 new_p = KafkaPC(**env_vars)
-print('created KafkaPC')
+print("created KafkaPC")
 
 for i in range(10):
 
-    message = {"count" : i}
+    message = {"count": i}
     new_p.send_msg(message)
-    print(f'Sent message {i}')
+    print(f"Sent message {i}")
     time.sleep(1)
