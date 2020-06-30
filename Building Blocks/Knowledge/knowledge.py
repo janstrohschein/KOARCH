d86db8acf018a80ff9eafc9815fb64fd6a938c88
janstrohschein <strohschein@gmx.net>
2020-06-30 16:24:06 +0200

    Created knowledge building block

---
+++ Building Blocks/Knowledge/knowledge.py
@@ -0,0 +1,53 @@
+import yaml
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import FileResponse, JSONResponse
+
+
+def import_knowledge(knowledge_path="knowledge.yaml"):
+
+    with open(knowledge_path, "r") as ymlfile:
+        knowledge = yaml.load(ymlfile, Loader=yaml.FullLoader)
+
+    return knowledge
+
+
+knowledgebase = FastAPI()
+
+knowledge = import_knowledge()
+
+
+@knowledgebase.get("/knowledge/")
+async def get_knowledge():
+    """ Returns the complete knowledgebase. """
+    response = JSONResponse(knowledge, status_code=200)
+    return response
+
+
+@knowledgebase.get("/knowledge/{usecase}/")
+async def get_usecase(usecase: str):
+    """ Filters the knowledge for a specific usecase. """
+    response = knowledge.get(usecase, "Key does not exist")
+    return response
+
+
+@knowledgebase.put("/import_knowledge/")
+async def import_knowledge(file: UploadFile = File(...)):
+    """ Imports a YAML file as new knowledgebase. """
+
+    content = await file.read()
+    await file.close()
+    content_yaml = yaml.load(content, Loader=yaml.FullLoader)
+    for key, value in content_yaml.items():
+        knowledge[key] = value
+
+    return JSONResponse(content={"knowledge": "updated"}, status_code=200)
+
+
+@knowledgebase.get("/export_knowledge/")
+async def export_knowledge():
+    """ Exports the current content of the knowledgebase into a YAML file."""
+
+    with open("knowledge_temp.yaml", "w") as ymlfile:
+        yaml.dump(knowledge, ymlfile)
+
+    return FileResponse(path="knowledge_temp.yaml", filename='knowledge.yaml')
