aaf886d8c1af86cbba3b1853d69a9dcc21bc0d4a
janstrohschein <strohschein@gmx.net>
2020-10-07 13:58:31 +0200

    adjusted plotting

--- Use_Cases/VPS_Popcorn_Production/Docker/src/L4_C_Reporting.py
+++ Use_Cases/VPS_Popcorn_Production/Docker/src/L4_C_Reporting.py
@@ -18,57 +18,84 @@ def forward_topic(msg):
     requests.post(url=URL, params=params)
 
 
-def plot_data(msg):
-    print('in plot_data')
+def plot_monitoring(msg):
+    # print("in plot_data")
     msgdata = new_c.decode_avro_msg(msg)
 
-    #plot tells if message is send as topic for plotData or plotMultipleData
-    #x_label is the label of xaxis
-    #x_data is data of xaxis
-    #x_int_to_date: set it True if your x_data is an integer-value, but you want to convert it to datetime
-    #y - yaxis-Data
-    new_data_point = {'plot': 'single',
-                       'x_label': 'id',
-                       'x_data': msgdata["id"],
-                       'x_int_to_date': False,
-                       'y': {"x": msgdata["x"],"y": msgdata["y"]}}
+    # plot tells if message is send as topic for plotData or plotMultipleData
+    # x_label is the label of xaxis
+    # x_data is data of xaxis
+    # x_int_to_date: set it True if your x_data is an integer-value, but you want to convert it to datetime
+    # y - yaxis-Data
+    new_data_point = {
+        "plot": "single",
+        "source": "monitoring",
+        "x_label": "id",
+        "x_data": msgdata["id"],
+        "x_int_to_date": False,
+        "y": {"x": msgdata["x"], "y": msgdata["y"]},
+    }
 
     new_c.send_msg(new_data_point)
-    print("message sent")
+    print("monitoring message sent")
+
+
+def plot_model_application(msg):
+
+    msgdata = new_c.decode_avro_msg(msg)
+    new_data_point = {
+        "plot": "single",
+        "source": "model_application",
+        "x_label": "id",
+        "x_data": msgdata["id"],
+        "x_int_to_date": False,
+        "y": {"pred_y": msgdata["pred_y"],
+              "rmse": msgdata["rmse"],
+              "CPU_ms": msgdata["CPU_ms"],
+              "RAM": msgdata["RAM"]},
+    }
+
+    new_c.send_msg(new_data_point)
+    print("model application message sent")
 
 
 def plot_data_multi(msg):
-    print('in plot_data_multi')
+    # print("in plot_data_multi")
     msgdata = new_c.decode_avro_msg(msg)
     splitData = msgdata["algorithm"].split("(")
 
-    #plot tells if message is send as topic for plotData or plotMultipleData
-    #x_label is the label of xaxis
-    #x_data is data of xaxis
-    #x_int_to_date: set it True if your x_data is an integer-value, but you want to convert it to datetime
-    #y - yaxis-Data
-    new_data_point = {'plot': 'multi',
-                       'multiplefilter': 'algorithm',
-                       'x_label': "id",
-                       'x_data': msgdata["id"],
-                       'x_int_to_date': False,
-                       'y': {"new_x": msgdata["new_x"], "algorithm": splitData[0]}}
+    # plot tells if message is send as topic for plotData or plotMultipleData
+    # x_label is the label of xaxis
+    # x_data is data of xaxis
+    # x_int_to_date: set it True if your x_data is an integer-value, but you want to convert it to datetime
+    # y - yaxis-Data
+    new_data_point = {
+        "plot": "multi",
+        "source": "model_evaluation",
+        "multiplefilter": "algorithm",
+        "x_label": "id",
+        "x_data": msgdata["id"],
+        "x_int_to_date": False,
+        "y": {"new_x": msgdata["new_x"], "algorithm": splitData[0]},
+    }
 
     new_c.send_msg(new_data_point)
     print("message sent")
 
 
-env_vars = {'config_path': os.getenv('config_path'),
-            'config_section': os.getenv('config_section')}
+env_vars = {
+    "config_path": os.getenv("config_path"),
+    "config_section": os.getenv("config_section"),
+}
 
 new_c = KafkaPC(**env_vars)
 
-api_dict = new_c.config['API_OUT']
-plot_dict = new_c.config['PLOT_TOPIC']
+api_dict = new_c.config["API_OUT"]
+plot_dict = new_c.config["PLOT_TOPIC"]
 
 
-API_URL = new_c.config['API_URL']
-ENDPOINT = new_c.config['API_ENDPOINT']
+API_URL = new_c.config["API_URL"]
+ENDPOINT = new_c.config["API_ENDPOINT"]
 
 for msg in new_c.consumer:
     # tests if msg.topic is in api_dict and calls function from dict
@@ -76,11 +103,15 @@ for msg in new_c.consumer:
         if api_dict.get(msg.topic) is not None:
             eval(api_dict[msg.topic])(msg)
     except Exception as e:
-        print(f"Processing Topic: {msg.topic} with Function: {api_dict[msg.topic]}\n Error: {e}")
+        print(
+            f"Processing Topic: {msg.topic} with Function: {api_dict[msg.topic]}\n Error: {e}"
+        )
 
     # tests if msg.topic is in plot_dict and calls function from dict
     try:
         if plot_dict.get(msg.topic) is not None:
             eval(plot_dict[msg.topic])(msg)
     except Exception as e:
-        print(f"Processing Topic: {msg.topic} with Function: {plot_dict[msg.topic]}\n Error: {e}")
+        print(
+            f"Processing Topic: {msg.topic} with Function: {plot_dict[msg.topic]}\n Error: {e}"
+        )
--- Use_Cases/VPS_Popcorn_Production/Docker/src/L4_plotData.py
+++ Use_Cases/VPS_Popcorn_Production/Docker/src/L4_plotData.py
@@ -3,53 +3,60 @@ import sys
 import json
 import numbers
 
-#Using plotly.express
+# Using plotly.express
 import plotly.graph_objects as go
 from datetime import datetime
 
-#sys.argv[1] = data (all database data)
-#sys.argv[2] = xaxis (name of datetime column in database)
-#sys.argv[3] = userName (name of userName column in database)
+# sys.argv[1] = data (all database data)
+# sys.argv[2] = xaxis (name of datetime column in database)
+# sys.argv[3] = userName (name of userName column in database)
 
 try:
-	#get data from sys.argv[1] and convert them into python dictionary
-	userData = json.loads(sys.argv[1])
-	#get keys from python dictionary in dataRowNames variable
-	dataRowNames = userData[0].keys()
-	#get dateRowName from sys.argv[2]
-	idRowName = sys.argv[2]
+    # get data from sys.argv[1] and convert them into python dictionary
+    userData = json.loads(sys.argv[1])
+    # get keys from python dictionary in dataRowNames variable
+    dataRowNames = userData[0].keys()
+    # get dateRowName from sys.argv[2]
+    idRowName = sys.argv[2]
 except IndexError:
-	sys.stderr.write("Wrong amount of arguments in sys.argv or no data")
-	exit(1)
+    sys.stderr.write("Wrong amount of arguments in sys.argv or no data")
+    exit(1)
 except json.JSONDecodeError:
-	sys.stderr.write("Data is not a JSON-String")
-	exit(2)
+    sys.stderr.write("Data is not a JSON-String")
+    exit(2)
 
-#format, in which date should be converted later
+# format, in which date should be converted later
 dateFormat = "%Y-%m-%d %H:%M:%S"
 
-#list of dates. Need this later to get the borders of graph (min/max)
+# list of dates. Need this later to get the borders of graph (min/max)
 idList = []
 for data in userData:
-	try:
-		idList.append(data[idRowName])
-	except KeyError:
-		sys.stderr.write("Given rowname of id in sys.argv[2] doesnt exist in data")
-		exit(4)
+    try:
+        idList.append(data[idRowName])
+    except KeyError:
+        sys.stderr.write("Given rowname of id in sys.argv[2] doesnt exist in data")
+        exit(4)
 
 
-#get dataRowNames that should be plotted
-dynamicNav = ''
-defaultDataRowname = ''
+# get dataRowNames that should be plotted
+dynamicNav = ""
+defaultDataRowname = ""
 for dataRowName in dataRowNames:
-	if dataRowName != idRowName and (len(sys.argv) < 4 or dataRowName!= sys.argv[3]):
-		if defaultDataRowname == '': 
-			defaultDataRowname = dataRowName
-		dynamicNav += '<li id="navElement-' + dataRowName + '" class="navElement"'
-		dynamicNav += """ onclick="showPlot('"""+dataRowName+"""');">"""+dataRowName+"""</li>"""
+    if dataRowName != idRowName and (len(sys.argv) < 4 or dataRowName != sys.argv[3]):
+        if defaultDataRowname == "":
+            defaultDataRowname = dataRowName
+        dynamicNav += '<li id="navElement-' + dataRowName + '" class="navElement"'
+        dynamicNav += (
+            """ onclick="showPlot('"""
+            + dataRowName
+            + """');">"""
+            + dataRowName
+            + """</li>"""
+        )
 
-#html-code of navbar
-print("""<!DOCTYPE html>
+# html-code of navbar
+print(
+    """<!DOCTYPE html>
 <html>
 <head>
 <style>
@@ -78,7 +85,9 @@ li {
 </head>
 <body>
 
-<ul id="navi" style="display: grid; grid-template-columns: repeat(auto-fit, 226px);">"""+dynamicNav+"""</ul>
+<ul id="navi" style="display: grid; grid-template-columns: repeat(auto-fit, 226px);">"""
+    + dynamicNav
+    + """</ul>
 <br><br>
 
 <div>
@@ -88,160 +97,193 @@ li {
 
 <script>
 function showPlot(plotKey) {
-	sessionStorage.setItem(window.location.href, plotKey);
-	var showElements = document.getElementsByClassName("visibleElements");
-	for (var i = 0; i < showElements.length; i++) {
-		showElements[i].style.display='none';
-	}
+    sessionStorage.setItem(window.location.href, plotKey);
+    var showElements = document.getElementsByClassName("visibleElements");
+    for (var i = 0; i < showElements.length; i++) {
+        showElements[i].style.display='none';
+    }
 
-	if (document.getElementsByClassName("active").length > 0) {
-		var previousActiveNavbarElement = document.getElementsByClassName("active")[0];
-		previousActiveNavbarElement.className = previousActiveNavbarElement.className.replace(" active", "");
-	}
-	var newActiveNavbarElement = document.getElementById(`navElement-${plotKey}`);
-	newActiveNavbarElement.className += " active";
+    if (document.getElementsByClassName("active").length > 0) {
+        var previousActiveNavbarElement = document.getElementsByClassName("active")[0];
+        previousActiveNavbarElement.className = previousActiveNavbarElement.className.replace(" active", "");
+    }
+    var newActiveNavbarElement = document.getElementById(`navElement-${plotKey}`);
+    newActiveNavbarElement.className += " active";
 
-	document.getElementById(plotKey).style.display='block';
-	window.dispatchEvent(new Event('resize'));
+    document.getElementById(plotKey).style.display='block';
+    window.dispatchEvent(new Event('resize'));
 }
 
 </script>
-""")
+"""
+)
 
 
-
-#create layout function
+# create layout function
 def createLayout(x_axisTitle, y_axisTitle):
-	return go.Layout(
-		xaxis=dict(
-			title=x_axisTitle
-		),
-		yaxis=dict(
-			title=y_axisTitle
-		) 
-	)
+    return go.Layout(xaxis=dict(title=x_axisTitle), yaxis=dict(title=y_axisTitle))
+
 
 # plot one User
 def plotData(userData, dataRowNames, idRowName, idList):
 
-	for dataRowName in dataRowNames:
+    for dataRowName in dataRowNames:
 
+        if dataRowName != idRowName:
 
-		if dataRowName != idRowName:
+            x_axisData = []
+            y_axisData = []
+            hoverData = []
+            for data in userData:
 
-			x_axisData = []
-			y_axisData = []
-			hoverData = []
-			for data in userData:
+                id = data[idRowName]
+                if isinstance(id, basestring):
+                    try:
+                        x_axisData.append(datetime.strptime(id, dateFormat))
+                    except ValueError:
+                        sys.stderr.write(
+                            "Given dates from date row doesnt match the format YYYY-MM-DD HH:MM:SS"
+                        )
+                        exit(3)
+                else:
+                    x_axisData.append(id)
+                # fill y_axisData with associated values
+                if (
+                    not isinstance(data[dataRowName], numbers.Number)
+                    and data[dataRowName] is not None
+                ):
+                    sys.stderr.write("Cannot plot non-numerical data.")
+                    exit(6)
+                y_axisData.append(data[dataRowName])
+                hoverString = ""
+                # fill hoverString with data that should be shown when mousehover a value in the graph
+                for key, value in data.items():
+                    hoverString += str(key) + ": " + str(value) + "<br>"
+                # <extra> is used to define a title for hovered data. If not empty, default is name of trace
+                hoverData.append(hoverString + "<extra></extra>")
 
-				id = data[idRowName]
-				if isinstance(id, basestring):
-					try:
-						x_axisData.append(datetime.strptime(id, dateFormat))
-					except ValueError:
-						sys.stderr.write("Given dates from date row doesnt match the format YYYY-MM-DD HH:MM:SS")
-						exit(3)
-				else:
-					x_axisData.append(id)
-				#fill y_axisData with associated values
-				if not isinstance(data[dataRowName], numbers.Number) and data[dataRowName] is not None:
-					sys.stderr.write("Cannot plot non-numerical data.")
-					exit(6)
-				y_axisData.append(data[dataRowName])
-				hoverString = ""
-				#fill hoverString with data that should be shown when mousehover a value in the graph
-				for key, value in data.items():
-					hoverString += str(key) + ": " + str(value) + "<br>"
-				#<extra> is used to define a title for hovered data. If not empty, default is name of trace
-				hoverData.append(hoverString + "<extra></extra>")
+            # create layout with given titles
+            layout = createLayout(idRowName, dataRowName)
 
-			#create layout with given titles
-			layout = createLayout(idRowName, dataRowName)
+            # create graph
+            fig = go.Figure(
+                data=[
+                    go.Scatter(
+                        x=x_axisData,
+                        y=y_axisData,
+                        hovertemplate=hoverData,
+                        mode="markers",
+                    )
+                ],
+                layout=layout,
+            )
 
-			#create graph
-			fig = go.Figure(data=[go.Scatter(x=x_axisData, y=y_axisData, hovertemplate=hoverData, mode="markers")], layout=layout)
-
-			#print the graph as html
-			print('<div id="'+dataRowName+'" class="visibleElements" style="display: none;">')
-			print(fig.to_html())
-			print("</div>")
+            # print the graph as html
+            print(
+                '<div id="'
+                + dataRowName
+                + '" class="visibleElements" style="display: none;">'
+            )
+            print(fig.to_html())
+            print("</div>")
 
 
 # plot multiple users in one Graph
 def plotMultipleData(userData, dataRowNames, idRowName, idList):
 
-	#get rowname of username column
-	userRowName = sys.argv[3]
-	userNames = set()
+    # get rowname of username column
+    userRowName = sys.argv[3]
+    userNames = set()
 
-	#fill the set with all usernames, no duplicates
-	for data in userData:
-		try:
-			userNames.add(data[userRowName])
-		except KeyError:
-			sys.stderr.write("Given rowname of username in sys.argv[3] doesnt exist in data")
-			exit(5)			
+    # fill the set with all usernames, no duplicates
+    for data in userData:
+        try:
+            userNames.add(data[userRowName])
+        except KeyError:
+            sys.stderr.write(
+                "Given rowname of username in sys.argv[3] doesnt exist in data"
+            )
+            exit(5)
 
-	for dataRowName in dataRowNames:
+    for dataRowName in dataRowNames:
 
-		if dataRowName != idRowName and dataRowName != userRowName:
+        if dataRowName != idRowName and dataRowName != userRowName:
 
-			layout = createLayout(idRowName, dataRowName)
+            layout = createLayout(idRowName, dataRowName)
 
-			#create empty layout
-			fig = go.Figure(layout=layout)
+            # create empty layout
+            fig = go.Figure(layout=layout)
 
-			for userName in userNames:
-				x_axisData = []
-				y_axisData = []
-				hoverData = []
+            for userName in userNames:
+                x_axisData = []
+                y_axisData = []
+                hoverData = []
 
-				for data in userData:
-					#check if data belongs to current username
-					if data[userRowName] == userName:
-						id = data[idRowName]
-						#fill x_axisData with associated values and convert them into datetime object
-						x_axisData.append(id)
-						#fill y_axisData with associated values
-						if not isinstance(data[dataRowName], numbers.Number) and data[dataRowName] is not None:
-							sys.stderr.write("Cannot plot non-numerical data.")
-							exit(6)
-						y_axisData.append(data[dataRowName])
-						hoverString = ""
-						#fill hoverString with data that should be shown when mousehover a value in the graph
-						for key, value in data.items():
-							hoverString += str(key) + ": " + str(value) + "<br>"
-						#<extra> is used to define a title for hovered data. If not empty, default is name of trace
-						hoverData.append(hoverString + "<extra></extra>")
+                for data in userData:
+                    # check if data belongs to current username
+                    if data[userRowName] == userName:
+                        id = data[idRowName]
+                        # fill x_axisData with associated values and convert them into datetime object
+                        x_axisData.append(id)
+                        # fill y_axisData with associated values
+                        if (
+                            not isinstance(data[dataRowName], numbers.Number)
+                            and data[dataRowName] is not None
+                        ):
+                            sys.stderr.write("Cannot plot non-numerical data.")
+                            exit(6)
+                        y_axisData.append(data[dataRowName])
+                        hoverString = ""
+                        # fill hoverString with data that should be shown when mousehover a value in the graph
+                        for key, value in data.items():
+                            hoverString += str(key) + ": " + str(value) + "<br>"
+                        # <extra> is used to define a title for hovered data. If not empty, default is name of trace
+                        hoverData.append(hoverString + "<extra></extra>")
 
-				#add trace to graph
-				fig.add_trace(go.Scatter(x=x_axisData, y=y_axisData, mode="markers", name=userName, hovertemplate=hoverData))
+                # add trace to graph
+                fig.add_trace(
+                    go.Scatter(
+                        x=x_axisData,
+                        y=y_axisData,
+                        mode="markers",
+                        name=userName,
+                        hovertemplate=hoverData,
+                    )
+                )
 
-			#print the graph as html
-			print('<div id="'+dataRowName+'" class="visibleElements" style="display: none; width:100%;">')
-			print(fig.to_html())
-			print("</div>")
+            # print the graph as html
+            print(
+                '<div id="'
+                + dataRowName
+                + '" class="visibleElements" style="display: none; width:100%;">'
+            )
+            print(fig.to_html())
+            print("</div>")
 
 
 if len(sys.argv) == 3:
-	plotData(userData, dataRowNames, idRowName, idList)
+    plotData(userData, dataRowNames, idRowName, idList)
 if len(sys.argv) == 4:
-	plotMultipleData(userData, dataRowNames, idRowName, idList)
+    plotMultipleData(userData, dataRowNames, idRowName, idList)
 
-print("""<script src="/socket.io/socket.io.js"></script>
+print(
+    """<script src="/socket.io/socket.io.js"></script>
 <script>
 if (sessionStorage.getItem(window.location.href) === null) {
-	showPlot('"""+defaultDataRowname+"""')
+    showPlot('"""
+    + defaultDataRowname
+    + """')
 }
 else {
-	showPlot(sessionStorage.getItem(window.location.href));
+    showPlot(sessionStorage.getItem(window.location.href));
 }
 var socket = io();
 socket.on("refresh", () => {
-	if (document.getElementById("checkbox").checked == true) {
-		window.location.reload();
-  	}
+    if (document.getElementById("checkbox").checked == true) {
+        window.location.reload();
+      }
 });
-</script>""")
+</script>"""
+)
 print("</body>")
 print("</html>")
