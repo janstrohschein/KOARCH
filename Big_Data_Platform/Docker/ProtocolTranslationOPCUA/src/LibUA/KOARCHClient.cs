c56b99004d92a1b188a97fee60d0000b110c1624
Andreas Bunte <andreas.bunte@th-owl.de>
2021-03-10 16:38:08 +0100

    Add protocol tranlation for OPC UA

---
+++ Big_Data_Platform/Docker/ProtocolTranslationOPCUA/src/LibUA/KOARCHClient.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using LibUA.Core;
+
+
+//This KOARCH client is an adapted version to the KOARCH project of the example of nauful from https://github.com/nauful/LibUA
+
+
+namespace LibUA
+{
+    public class Program
+    {
+        class KOARCHClient : Client
+        {
+
+            X509Certificate2 appCertificate = null;
+            RSACryptoServiceProvider cryptPrivateKey = null;
+
+            public override X509Certificate2 ApplicationCertificate
+            {
+                get { return appCertificate; }
+            }
+
+            public override RSACryptoServiceProvider ApplicationPrivateKey
+            {
+                get { return cryptPrivateKey; }
+            }
+
+            private void LoadCertificateAndPrivateKey()
+            {
+                try
+                {
+                    // Try to load existing (public key) and associated private key
+                    appCertificate = new X509Certificate2("ClientCert.der");
+                    cryptPrivateKey = new RSACryptoServiceProvider();
+
+                    var rsaPrivParams = UASecurity.ImportRSAPrivateKey(File.ReadAllText("ClientKey.pem"));
+                    cryptPrivateKey.ImportParameters(rsaPrivParams);
+                }
+                catch
+                {
+                    // Make a new certificate (public key) and associated private key
+                    var dn = new X500DistinguishedName("CN=Client certificate;OU=Demo organization",
+                        X500DistinguishedNameFlags.UseSemicolons);
+                    SubjectAlternativeNameBuilder sanBuilder = new SubjectAlternativeNameBuilder();
+                    sanBuilder.AddUri(new Uri("urn:DemoApplication"));
+
+                    using (RSA rsa = RSA.Create(2048))
+                    {
+                        var request = new CertificateRequest(dn, rsa, HashAlgorithmName.SHA256,
+                            RSASignaturePadding.Pkcs1);
+
+                        request.CertificateExtensions.Add(sanBuilder.Build());
+
+                        var certificate = request.CreateSelfSigned(new DateTimeOffset(DateTime.UtcNow.AddDays(-1)),
+                            new DateTimeOffset(DateTime.UtcNow.AddDays(3650)));
+
+                        appCertificate = new X509Certificate2(certificate.Export(X509ContentType.Pfx, ""),
+                            "", X509KeyStorageFlags.DefaultKeySet);
+
+                        var certPrivateParams = rsa.ExportParameters(true);
+                        File.WriteAllText("ClientCert.der", UASecurity.ExportPEM(appCertificate));
+                        File.WriteAllText("ClientKey.pem", UASecurity.ExportRSAPrivateKey(certPrivateParams));
+
+                        cryptPrivateKey = new RSACryptoServiceProvider();
+                        cryptPrivateKey.ImportParameters(certPrivateParams);
+                    }
+                }
+            }
+
+            public KOARCHClient(string Target, int Port, int Timeout)
+                : base(Target, Port, Timeout)
+            {
+                LoadCertificateAndPrivateKey();
+            }
+
+            public override void NotifyDataChangeNotifications(uint subscrId, uint[] clientHandles, DataValue[] notifications)
+            {
+                for (int i = 0; i < clientHandles.Length; i++)
+                {
+                    Console.WriteLine("subscrId {0} handle {1}: {2}", subscrId, clientHandles[i], notifications[i].Value.ToString());
+                }
+            }
+
+            public override void NotifyEventNotifications(uint subscrId, uint[] clientHandles, object[][] notifications)
+            {
+                for (int i = 0; i < clientHandles.Length; i++)
+                {
+                    Console.WriteLine("subscrId {0} handle {1}: {2}", subscrId, clientHandles[i], string.Join(",", notifications[i]));
+                }
+            }
+        }
+
+        static ApplicationDescription[] appDescs = null;
+        static EndpointDescription[] endpointDescs = null;
+        static Boolean initilized = false;
+        public static LibUA.Client OPCConnectToServer(String url)
+        {
+            Console.WriteLine("OPC UA URL: " + url);
+            LibUA.Client client = null;
+            if (url.Contains("opc.tcp://"))
+            {
+                url = url.Replace("opc.tcp://", "");
+            }
+            String[] ip = url.Split(":");
+            if (!initilized)
+            {
+                Console.WriteLine("Connecting");
+                client = new KOARCHClient(ip[0], int.Parse(ip[1]), 1000);
+                client.Connect();
+                Console.WriteLine("OPC UA is connected: " + client.IsConnected);
+                client.OpenSecureChannel(MessageSecurityMode.None, SecurityPolicy.None, null);
+                client.FindServers(out appDescs, new[] { "en" });
+                client.GetEndpoints(out endpointDescs, new[] { "en" });
+                client.Disconnect();
+                initilized = true;
+            }
+
+            var appDesc = new ApplicationDescription(
+            "urn:KORCHDemoApplication", "uri:KOARCHDemoApplication", new LocalizedText("OPC UA Client"),
+            ApplicationType.Client, null, null, null);
+
+            var connectRes = client.Connect();
+            var openRes = client.OpenSecureChannel(MessageSecurityMode.None, SecurityPolicy.None, null);
+            var createRes = client.CreateSession(appDesc, "urn:DemoApplication", 120);
+            var activateRes = client.ActivateSession(new UserIdentityAnonymousToken("0"), new[] { "en" });
+            return client;
+        }
+
+        public static Object[] OPCReadNotes(LibUA.Client client, Collection<String> nodes)
+        {
+            DataValue[] dvs = null;
+            ReadValueId[] node = new ReadValueId[nodes.Count];
+            for (int i = 0; i < node.Length; i++)
+            {
+                node[i] = new ReadValueId(new NodeId(4, nodes[i]), NodeAttribute.Value, null, new QualifiedName(0, null));
+            }
+            var readRes = client.Read(node, out dvs);
+
+            return dvs;
+        }
+
+        public static void OPCSubscribe(LibUA.Client client, Collection<String> nodes)
+        {
+            uint subscrId;
+            client.CreateSubscription(50, 1000, true, 0, out subscrId);
+            uint clientHandleEventMonitor = 0;
+
+            MonitoredItemCreateRequest[] monitor = new MonitoredItemCreateRequest[nodes.Count];
+            ReadValueId[] node = new ReadValueId[nodes.Count];
+            for (int i = 0; i < node.Length; i++)
+            {
+                node[i] = new ReadValueId(new NodeId(4, nodes[i]), NodeAttribute.Value, null, new QualifiedName(0, null));
+                monitor[i] = new MonitoredItemCreateRequest(node[i], MonitoringMode.Reporting, new MonitoringParameters(clientHandleEventMonitor, 100, null, 100, true));
+            }
+
+            MonitoredItemCreateResult[] monitorCreateResults;
+            client.CreateMonitoredItems(subscrId, TimestampsToReturn.Both, monitor, out monitorCreateResults);
+
+
+            Console.WriteLine("Start Monitoring Items");
+            Console.ReadKey();
+
+            client.Dispose();
+            Console.WriteLine("is connected: " + client.IsConnected);
+        }
+    }
+}
---
+++ Big_Data_Platform/Docker/ProtocolTranslationOPCUA/src/ProtocolTranslationDocker/Kafka.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Avro;
+using Avro.Generic;
+using Confluent.SchemaRegistry.Serdes;
+using Confluent.SchemaRegistry;
+using System.Collections.Generic;
+using Confluent.Kafka.SyncOverAsync;
+using static OPCUAGateway.OPCConnection;
+using Confluent.Kafka;
+using System.Threading;
+using Confluent.Kafka.Admin;
+
+namespace OPCUAGateway
+{
+    public class KafkaConfig
+    {
+        public String path_to_schema { get; set; } = @"CPPSdataGeneric.avsc";
+        public String bootstrap_servers { get; set; } = "localhost:9092";
+        public String schema_registry_url { get; set; } = "http://localhost:8081";
+        public String topic_name { get; set; } = "CPPSdata";
+        public String groupName { get; set; } = "print_count";
+    }
+    class Kafka
+    {
+        /// <summary>
+        /// Test function for adaption, whichs sends a message without a schema.
+        /// </summary>
+        /// <param name="kConfig">Configuration of Kafka</param>
+        /// <param name="message">Messeage that should be send, in our Example "Signalname:Value"</param>
+        /// <returns></returns>
+        public static Object sendMessageWithoutSchema(KafkaConfig kConfig, string message)
+        {
+            ProducerConfig config = new ProducerConfig{ BootstrapServers = kConfig.bootstrap_servers };
+            using (var producer =
+                 new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    return producer.ProduceAsync(kConfig.topic_name, new Message<Null, string> { Value = message })
+                        .GetAwaiter()
+                        .GetResult();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Oops, something went wrong: {e}");
+                }
+            }
+            return null;
+        }
+        /// <summary>
+        /// Creates a producer that send messages with a certain avro schema.
+        /// </summary>
+        /// <param name="kConfig">Configuration of the Kafka consumer </param>
+        /// <param name="record">Message that will be send</param>
+        public static async void sendMessage(KafkaConfig kConfig, GenericRecord record)
+        {
+            // Producer configuration, make sure port and ip work, set also do ipv4 to avoid ipv6 errors
+            var config = new Confluent.Kafka.ProducerConfig
+            {
+                BootstrapServers = kConfig.bootstrap_servers,
+                BrokerAddressFamily = Confluent.Kafka.BrokerAddressFamily.V4,
+            };
+
+            // Producer schema configuration, make sure kafka provides a schema url
+            var schema = new Confluent.SchemaRegistry.SchemaRegistryConfig
+            {
+                Url = kConfig.schema_registry_url
+            };
+
+            // Setting up the producer with the schema
+            using (var schemaRegistry = new CachedSchemaRegistryClient(schema))
+            using (var producer = new Confluent.Kafka.ProducerBuilder<Confluent.Kafka.Null, GenericRecord>(config)
+                    .SetValueSerializer(new AvroSerializer<GenericRecord>(schemaRegistry))
+                    .Build())
+            {
+                // Try to produce a message
+                try
+                {
+                    var dr = await producer
+                    .ProduceAsync(kConfig.topic_name, new Confluent.Kafka.Message<Confluent.Kafka.Null, GenericRecord> { Value = record });
+                    Console.WriteLine("Produced to: {0}", dr.TopicPartitionOffset);
+                }
+                catch (Confluent.Kafka.ProduceException<Confluent.Kafka.Null, GenericRecord> ex)
+                {
+                    Console.WriteLine(ex);
+                }
+                // Delete the queue 
+                producer.Flush();
+            }
+        }
+
+        /// <summary>
+        /// Creates a consumer that listen to a certain topic and use the content to adapt the OPC UA server that is specified by the parameters of this method.
+        /// </summary>
+        /// <param name="kConfig">Configuration of the Kafka consumer</param>
+        /// <param name="adaptionConfig">Configuration of the OPC UA Server, where updates arriving by kafka messages are provided to</param>
+        public static void RunConsumerWithoutSchema(KafkaConfig kConfig, OPC adaptionConfig)
+        {
+            CheckTopicAvailability(kConfig.bootstrap_servers, kConfig.topic_name);
+            // Consumer configuration, make sure port and ip work, set also do ipv4 to avoid ipv6 errors
+            var config = new Confluent.Kafka.ConsumerConfig
+            {
+                BootstrapServers = kConfig.bootstrap_servers,
+                BrokerAddressFamily = Confluent.Kafka.BrokerAddressFamily.V4,
+                GroupId = kConfig.groupName,
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+            var consumeTask = Task.Run(() =>
+            {
+                using (var builder = new ConsumerBuilder<Ignore,
+                string>(config).Build())
+                {
+                    builder.Subscribe(kConfig.topic_name);
+                    var cancelToken = new CancellationTokenSource();
+                    try
+                    {
+                        while (true)
+                        {
+                            var consumer = builder.Consume(cancelToken.Token);
+                            Console.WriteLine($"Message: {consumer.Message.Value} received from {consumer.TopicPartitionOffset}");
+                            String[] signals = consumer.Message.Value.Split(":");
+                            adaptOPC(adaptionConfig, signals);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.Error.WriteLine("Consumer Error: " + e);
+                        builder.Close();
+                    }
+                }
+            });
+        }
+
+        /// <summary>
+        /// Method checks, if the topic is availabe at the bootstrap server and in not the topic is created to prevent an error at the first occurence
+        /// </summary>
+        /// <param name="bootstrapServers">Adress of the bootstrap server</param>
+        /// <param name="topicName">Name of the topic that should be checked</param>
+        static async void CheckTopicAvailability(string bootstrapServers, string topicName)
+        {
+            using (var adminClient = new AdminClientBuilder(new AdminClientConfig { BootstrapServers = bootstrapServers }).Build())
+            {
+                Boolean topicExists = false;
+                try
+                {
+                    Metadata md = adminClient.GetMetadata(topicName, new TimeSpan(0, 0, 1, 0));
+                    List<TopicMetadata> topics = md.Topics;
+                    for (int i = 0; i < topics.Count; i++)
+                    {
+                        if (topics[i].Topic.Equals(topicName))
+                        {
+                            topicExists = true;
+                        }
+                    }
+                }
+                catch(Exception e)
+                {
+                    Console.Error.WriteLine(e);
+                }
+                if (!topicExists) { 
+                try
+                {
+                    await adminClient.CreateTopicsAsync(new TopicSpecification[] {
+                        new TopicSpecification { Name = topicName, ReplicationFactor = 1, NumPartitions = 1 } });
+                }
+                catch (CreateTopicsException e)
+                {
+                    Console.WriteLine($"An error occured creating topic {e.Results[0].Topic}: {e.Results[0].Error.Reason}");
+                }
+            }
+            }
+        }
+    }
+}
+
+
